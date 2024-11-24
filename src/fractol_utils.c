fefcdfc965224df8777f2926a7d9b8fc444c1585
Abdulbaki <Salausmart132@gmail.com>
2024-11-24 20:07:40 +0200

    just fixing fixing

--- src/fractol_utils.c
+++ src/fractol_utils.c
@@ -1,26 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.h                                          :+:      :+:    :+:   */
+/*   fractol_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sabdulba <sabdulba@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/21 17:20:39 by sabdulba          #+#    #+#             */
-/*   Updated: 2024/11/24 20:06:15 by sabdulba         ###   ########.fr       */
+/*   Created: 2024/11/23 20:04:00 by sabdulba          #+#    #+#             */
+/*   Updated: 2024/11/24 20:06:59 by sabdulba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRACTOL_H
-# define FRACTOL_H
+#include "../inc/fractol.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-#include "../lib/libft/inc/libft.h"
-//#include "../lib/libft/inc/ft_printf.h"
-#include "../lib/MLX42/include/MLX42/MLX42.h"
-
-void	ft_error(void);
-
-# endif
+void	ft_error(void)
+{
+	ft_printf("Error occured");
+	//ft_putstr_fd("Error occured", 1);
+}
