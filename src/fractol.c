8e546ca3c28c141e39fdcb2547e6149b4f67721e
Abdulbaki <Salausmart132@gmail.com>
2024-11-25 11:55:20 +0200

    i dont know what im foing yet

--- src/fractol.c
+++ src/fractol.c
@@ -6,7 +6,7 @@
 /*   By: sabdulba <sabdulba@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 17:22:49 by sabdulba          #+#    #+#             */
-/*   Updated: 2024/11/23 20:08:15 by sabdulba         ###   ########.fr       */
+/*   Updated: 2024/11/25 11:53:45 by sabdulba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,11 @@ int main(void)
   //window = mlx_new_window(mlx, 1000, 1000, "Title");
   img = mlx_new_image(mlx, 128, 128);
   if (!img)
-	ft_error();
+  	ft_error();
+  mlx_image_to_window(mlx, img, 0, 0);
+  mlx_put_pixel(img, 20, 20, 0x00FF0000);
+  
   mlx_loop(mlx);
+  mlx_terminate(mlx);
   return (0);
 }
